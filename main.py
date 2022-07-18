18dd9b89abd0dd945acafab608bc5f5f7fae28aa
Mango <evan940415@gmail.com>
2022-07-18 17:19:24 +0800

    remake: main.py

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
-import os
+import discord
 from discord.ext import commands
+from discord.ext.commands import Command, Context
 
-import utils.func as utils
+import os
+import dotenv
 
-config = utils.jload(file='./config', mode='r', encoding='utf8')
+dotenv.load_dotenv()
+    
+class MyBot(commands.Bot):
+    def __init__(self, command_prefix="mb.", intents=discord.Intents.all()) -> None:
+        super().__init__(command_prefix=command_prefix, intents=intents)
+        self._add_command()
 
-bot = commands.Bot(
-    command_prefix = config['commands.prefix'],
-    Intents = utils.getIntents(config['commands.intents'])
-    )
+        # For Pylance
+        self.bot: commands.Bot
 
-@bot.event
-async def on_ready():
-    print('> Bot is Ready')
-    for filename in os.listdir('./cogs'):
-        if filename[-3:] == '.py':
-            bot.load_extension(f'cogs.{filename}')
+    @property
+    def unpreload(self):
+        return set()
 
-bot.run(utils.jload(file='./token'))
+    async def on_ready(self):
+        print(f"Python >> Bot is Ready, Login: {self.user}")
+        for dir_cog in os.listdir("./cogs"):
+            for cog in os.listdir(f"./cogs/{dir_cog}"):
+                if cog.endswith(".py") and cog not in self.unpreload:
+                    self.load_extension(f"cogs.{dir_cog}.{cog.removesuffix('.py')}")
+                    print(f">>> Load Cog: cogs.{dir_cog}.{cog.removeprefix('.py')}")
+    
+    def _add_command(self):
+        @self.command()
+        async def cogs(ctx:Context, cog_name:str, mode:str="load") -> Command:
+            path = cog_name.removesuffix(".py").replace("/", ".").replace("\\", ".")
+            match mode.lower():
+                case "load" | "l":
+                    self.load_extension(path)
+                    mode = "load"
+                case "unload" | "u":
+                    self.unload_extension(path)
+                    mode = "unload"
+                case "reload" | "r":
+                    self.reload_extension(path)
+                    mode = "reload"
+                case _:
+                    return await ctx.send(f"> ERROR: mode {mode} is undefined")
+            await ctx.send(f"> SUCCESS: cog {cog_name} is has succeeded {mode}")
+
+myBot = MyBot()
+myBot.run(os.getenv("TOKEN"))
